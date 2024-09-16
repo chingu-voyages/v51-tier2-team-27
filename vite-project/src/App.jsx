f3731a0cb25ca0b6e15cad68b6e7187f0c558eee
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-16 10:34:50 -0600

    create state variable to control modal open/close and pass to child component as prop

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react'
 import './App.css'
+import CreateGroup from './components/CreateGroup';
 
 
 function App() {
-  
+  const [addGroupModalIsOpen, setAddGroupModalIsOpen] = useState(true);
 
   return (
-    <>        
+    <>  
+    <CreateGroup
+    addGroupModalIsOpen={addGroupModalIsOpen}
+    />    
     </>
   )
 }
