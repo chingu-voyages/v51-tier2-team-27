6275d8d6c68f1b01ee91b932999f159b749c58bc
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-16 09:24:20 -0600

    add pseudo code for readability and render groups list using child group component

--- vite-project/src/components/DisplayGroupList.jsx
+++ vite-project/src/components/DisplayGroupList.jsx
@@ -1,25 +1,50 @@
 import React from 'react'
+import DisplayGroup from './DisplayGroup'
 
-export default function DisplayGroupList() {
-    const groupsList = [
+export default function DisplayGroupList(props) {
+    // fake data used to create display list
+    // data will eventually come from top level App component
+    // and will be managed retrieved from local storage
+    const groupsListData = [
         {
             groupId: 1,
             groupName: "test1",
             groupDescription: "test est eatoiehtapoehgpauehg",
-            groupBudget: 2000
+            groupBudget: 2000,
+            numGroupMembers: 4
         },
         {
             groupId: 2,
             groupName: "test2",
             groupDescription: "test est eatoiehtapoehgpauehg",
-            groupBudget: 1000
+            groupBudget: 1000,
+            numGroupMembers: 3
         }
     ]
+
+    // map over groupsList
+    // render each DisplayGroup by sending props
+    const groups = groupsListData.map(
+        group => {
+            return (
+                <DisplayGroup
+                groupId={group.groupId}
+                groupName={group.groupName}
+                groupDescription={group.groupDescription}
+                groupBudget={group.groupBudget}
+                numGroupMembers={group.numGroupMembers}
+                />
+            )
+        }
+    )
+    
 
 
     return (
         <div>
-        
+           <h2>Groups</h2>
+           <button>Add Group</button> 
+           {groups}
         </div>
   )
 }
