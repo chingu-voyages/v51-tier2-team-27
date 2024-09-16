72499c31f79a9a241908226c5b7f6486fde1fae1
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-16 10:17:25 -0600

    conditionally render create group modal based on state variable

--- vite-project/src/components/CreateGroup.jsx
+++ vite-project/src/components/CreateGroup.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function CreateGroup() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function CreateGroup(props) {
+  
+  if (props.addGroupModalIsOpen) {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+  
+  
 }
