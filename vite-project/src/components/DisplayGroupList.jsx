dacbd8f08364712aea54cd4423e0a24af3396a05
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-16 09:05:31 -0600

    add fake groups data

---
+++ vite-project/src/components/DisplayGroupList.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function DisplayGroupList() {
+    const groupsList = [
+        {
+            groupId: 1,
+            groupName: "test1",
+            groupDescription: "test est eatoiehtapoehgpauehg",
+            groupBudget: 2000
+        },
+        {
+            groupId: 2,
+            groupName: "test2",
+            groupDescription: "test est eatoiehtapoehgpauehg",
+            groupBudget: 1000
+        }
+    ]
+
+
+    return (
+        <div>
+        
+        </div>
+  )
+}
