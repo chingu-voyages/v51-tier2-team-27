e1126993d20c903745338f035bc45aa2a66da688
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-16 10:04:16 -0600

    move data to App and create function for opening modal

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,74 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 import DisplayGroupList from './components/DisplayGroupList'
 
 
 function App() {
+
+  const [groupsData, setGroupsData] = useState(/* () => JSON.parse(localStorage.getItem('FairShare_groupsData')) ||  */
+  [
+    {
+        groupId: 1,
+        groupName: "test1",
+        groupDescription: "test est eatoiehtapoehgpauehg",
+        groupBudget: 2000,
+        groupMembers: [
+          {
+            name: "person's name",
+            avatar: "path to avatar image",
+            id: 1
+          },
+          {
+            name: "tony the tiger",
+            avatar: "path to avatar image2",
+            id: 2
+          }
+        ]
+    },
+    {
+        groupId: 2,
+        groupName: "test2",
+        groupDescription: "test est eatoiehtapoehgpauehg",
+        groupBudget: 1000,
+        groupMembers: [
+          {
+            name: "jane doe",
+            avatar: "path to avatar image",
+            id: 4
+          },
+          {
+            name: "team member 2",
+            avatar: "path to avatar image2",
+            id: 5
+          },
+          {
+            name: "frankie frank",
+            avatar: "path to avatar image4",
+            id: 6
+          }
+        ]
+    }
+  ]);
+
+  const [addGroupModalIsOpen, setAddGroupModalIsOpen] = useState(false);
+
+
+  useEffect(() => {
+    localStorage.setItem('FairShare_groupsData', JSON.stringify(groupsData));
+  }, [groupsData]);
+
+  function openAddGroupModal() {
+    setAddGroupModalIsOpen(true);
+  }
   
 
   return (
-    <>
-    <DisplayGroupList/>        
-    </>
+    <div>
+      <DisplayGroupList
+      groupsData={groupsData}
+      openAddGroupModal={openAddGroupModal}
+      />        
+    </div>
   )
 }
 
--- vite-project/src/components/DisplayGroupList.jsx
+++ vite-project/src/components/DisplayGroupList.jsx
@@ -2,29 +2,8 @@ import React from 'react'
 import DisplayGroup from './DisplayGroup'
 
 export default function DisplayGroupList(props) {
-    // fake data used to create display list
-    // data will eventually come from top level App component
-    // and will be managed retrieved from local storage
-    const groupsListData = [
-        {
-            groupId: 1,
-            groupName: "test1",
-            groupDescription: "test est eatoiehtapoehgpauehg",
-            groupBudget: 2000,
-            numGroupMembers: 4
-        },
-        {
-            groupId: 2,
-            groupName: "test2",
-            groupDescription: "test est eatoiehtapoehgpauehg",
-            groupBudget: 1000,
-            numGroupMembers: 3
-        }
-    ]
 
-    // map over groupsList
-    // render each DisplayGroup by sending props
-    const groups = groupsListData.map(
+    const groups = props.groupsData.map(
         group => {
             return (
                 <DisplayGroup
@@ -32,18 +11,17 @@ export default function DisplayGroupList(props) {
                 groupName={group.groupName}
                 groupDescription={group.groupDescription}
                 groupBudget={group.groupBudget}
-                numGroupMembers={group.numGroupMembers}
+                numGroupMembers={group.groupMembers.length}
+                key={group.groupId}
                 />
             )
         }
     )
-    
-
 
     return (
         <div>
            <h2>Groups</h2>
-           <button>Add Group</button> 
+           <button onClick={props.openAddGroupModal}>Add Group</button> 
            {groups}
         </div>
   )
