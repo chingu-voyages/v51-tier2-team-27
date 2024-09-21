ec5812cbaa2854191bba93f354926130769c33ba
plaseo <118751105+plaseo@users.noreply.github.com>
2024-09-21 16:05:05 -0500

    small tweaks

--- vite-project/src/landing/LandingBottom.jsx
+++ vite-project/src/landing/LandingBottom.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import Wave from '../assets/home-bg-wave.png';
+import Wave from "../assets/home-bg-wave.png";
 
 export default function LandingBottom() {
   return (
-    <div className="bg-center w-full h-screen relative" style={{ backgroundImage: `url(${Wave})`, backgroundRepeat: 'no-repeat', backgroundSize: 'contain' }}>
-      
-      <div className="bg-center w-full h-screen flex flex-col items-center justify-center" style={{ backgroundImage: `url(${Wave})`, backgroundRepeat: 'no-repeat', backgroundSize: 'contain' }}>
-        <h1 className="text-4xl font-bold text-black">What Are You Waiting For?</h1>
-        <p className="mt-4 text-lg text-black">Create your first expense group!</p>
-        <button className="bg-[#E3315D] text-white py-2 px-4 rounded-lg text-sm">
-          Get Started
-        </button>
-    </div>
-      
+    <div
+      className="bg-center w-full h-96 flex flex-col items-center justify-center"
+      style={{
+        backgroundImage: `url(${Wave})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "contain",
+      }}
+    >
+      <h1 className="text-4xl font-bold text-black">
+        What Are You Waiting For?
+      </h1>
+      <p className="mt-4 text-lg text-black">
+        Create your first expense group!
+      </p>
+      <button className="bg-[#E3315D] text-white py-2 px-4 rounded-lg text-sm">
+        Get Started
+      </button>
     </div>
   );
 }
--- vite-project/src/landing/LandingMiddle.jsx
+++ vite-project/src/landing/LandingMiddle.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Arrow from '../assets/arrow-pink.png';
 
 export default function LandingMiddle() {
