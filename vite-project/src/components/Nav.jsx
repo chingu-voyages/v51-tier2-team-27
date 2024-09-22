c5cfed147cffac17910bf80a7900071128bcd055
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-22 09:15:31 -0600

    style buttons - spacing and sizing

--- vite-project/src/components/Nav.jsx
+++ vite-project/src/components/Nav.jsx
@@ -5,12 +5,12 @@ export default function Nav(props) {
 
     if (!props.addGroupModalIsOpen) {
         return (
-            <div className='flex flex-col col-span-1 bg-teal p-4'>
+            <div className='flex flex-col col-span-2 bg-teal p-4 items-center'>
               <img src={logo} className=''/> 
-              <p>Start sharing expenses easily with FairShare!</p>
-              <button>Groups</button>
-              <button>Expenses</button>
-              <button>Analytics</button>
+              <p className='my-10'>Start sharing expenses easily with FairShare!</p>
+              <button className='my-10  bg-beige text-button w-5/6 py-2'>Groups</button>
+              <button className=' bg-beige text-button w-5/6 py-2'>Expenses</button>
+              <button  className='my-10 bg-beige text-button w-5/6 py-2'>Analytics</button>
             </div>
           )
     }
