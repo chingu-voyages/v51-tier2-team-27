8cc19dd3f683254bf4eb62e07192fd62aefd2dbd
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-22 10:20:17 -0600

    add styling and onClick update selected nav element function

--- vite-project/src/components/Nav.jsx
+++ vite-project/src/components/Nav.jsx
@@ -7,10 +7,10 @@ export default function Nav(props) {
         return (
             <div className='flex flex-col col-span-2 bg-teal p-4 items-center'>
               <img src={logo} className=''/> 
-              <p className='my-10'>Start sharing expenses easily with FairShare!</p>
-              <button className='my-10  bg-beige text-button w-5/6 py-2'>Groups</button>
-              <button className=' bg-beige text-button w-5/6 py-2'>Expenses</button>
-              <button  className='my-10 bg-beige text-button w-5/6 py-2'>Analytics</button>
+              <p className='my-10 text-center font-bold'>Start sharing expenses easily with FairShare!</p>
+              <button onClick={() => props.updateNav('groups')} className='shadow my-10  bg-beige text-button w-5/6 py-2'>Groups</button>
+              <button onClick={() => props.updateNav('expenses')} className='shadow bg-beige text-button w-5/6 py-2'>Expenses</button>
+              <button onClick={() => props.updateNav('analytics')} className='shadow my-10 bg-beige text-button w-5/6 py-2'>Analytics</button>
             </div>
           )
     }
