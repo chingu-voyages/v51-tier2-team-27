a5de664f06ee1bc56806126898d24aa42980af49
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-22 08:30:57 -0600

    create state variable for user nav preference and retrieve from local storage upon load

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,11 +4,12 @@ import HomeScreen from './screens/HomeScreen'
 import CreateGroup from './components/Groups/CreateGroup';
 import DisplayGroupList from './components/Groups/DisplayGroupList'
 import Footer from './components/Footer'
+import Nav from './components/Nav'
 
 function App() {
   const [addGroupModalIsOpen, setAddGroupModalIsOpen] = useState(false);
   const [groupsData, setGroupsData] = useState(() => JSON.parse(localStorage.getItem('FairShare_groupsData')) || []); 
-  
+  const [navSelect, setNavSelect] = useState(() => JSON.parse(localStorage.getItem('FairShare_navSelect')) || 'groups');
   
   useEffect(() => {
     localStorage.setItem('FairShare_groupsData', JSON.stringify(groupsData));
@@ -42,17 +43,20 @@ function App() {
   return (
     <>  
     {/* <HomeScreen />   */}
+    <Nav 
+    addGroupModalIsOpen={addGroupModalIsOpen}
+    />
     <CreateGroup
     addGroupModalIsOpen={addGroupModalIsOpen}
     addGroup={addGroup}
     closeAddGroupModal={closeAddGroupModal}
     />   
     <DisplayGroupList
-      groupsData={groupsData}
-      setGroupsData={setGroupsData}
-      openAddGroupModal={openAddGroupModal}
-      deleteGroup={deleteGroup}
-      addGroupModalIsOpen={addGroupModalIsOpen}
+    groupsData={groupsData}
+    setGroupsData={setGroupsData}
+    openAddGroupModal={openAddGroupModal}
+    deleteGroup={deleteGroup}
+    addGroupModalIsOpen={addGroupModalIsOpen}
     />
     {/* <Footer /> */}
     </>
