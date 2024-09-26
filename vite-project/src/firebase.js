4018aa530194b864c2297e3e9724d84d1cf55f0f
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-09-26 10:21:01 -0500

    Setup Firebase for uploading receipts to creat group modal.

--- vite-project/src/firebase.js
+++ vite-project/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "expense-tracker-e91b8.firebaseapp.com",
-  projectId: "expense-tracker-e91b8",
-  storageBucket: "expense-tracker-e91b8.appspot.com",
-  messagingSenderId: "840875982038",
-  appId: "1:840875982038:web:08b8b82928dfc0c7505d77"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "splithubstars.firebaseapp.com",
+  projectId: "splithubstars",
+  storageBucket: "splithubstars.appspot.com",
+  messagingSenderId: "856025492605",
+  appId: "1:856025492605:web:2b4682edb83eae5be85514"
 };
 
 // Initialize Firebase
